d1799da2add866c14d564590240d574ccca7e004
Benfarhat <benfarhat.elyes@gmail.com>
2018-03-19 18:18:36 +0100

    implementing passport strategies

--- router.js
+++ router.js
@@ -1,5 +1,11 @@
-const Authentitcation = require('./controllers/authentication')
+const Authentication = require('./controllers/authentication')
+const passportService = require('./services/passport')
+const passport = require('passport')
+
+const requireAuth = passport.authenticate('jwt', { session: false })
 
 module.exports = app => {
-    app.post('/signup', Authentitcation.signup)
+    app.get('/', requireAuth, (req, res) => res.send({ authorization: 'done'}))
+    
+    app.post('/signup', Authentication.signup)
 }
--- services/passport.js
+++ services/passport.js
@@ -3,9 +3,63 @@ const User = require('../models/user')
 const config = require('../config')
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
+const LocalStrategy = require('passport-local')
+
+/*
+Auth Flow
+
+    * Signing Up
+        --> Verify email is not in use create pwd with brypt
+            --> Create and return Token
+
+    * Signing In      
+        --> Verify Email/Password With LOCAL STRATEGY (find user and compare password with bcrypt)
+            --> Find and return Token
+
+    * Auth's Request  
+        --> Verify Token with JWT STRATEGY
+            --> Allow Resource Access
+
+*/
+
+// -======================-
+// -=== LOCAL STRATEGY ===- 
+// -======================-
+
+// Setup options for Local Strategy
+const localOptions = {
+    usernameField: 'email', // default to 'username'
+    passwordField: 'password' // default to 'password'
+}
+// Create local strategy
+const localLogin = new LocalStrategy(localOptions, (email, password, done) => {
+    // Verify this username and password, call done ith the user
+    // if it's the correct username and password
+    // otherwise, call done with false
+    User.findOne({ email }, (err, user) => {
+        if (err) { return done(err, false) }
+
+        if(!user) {
+            done(null, false)
+        } else {
+            // compare passwords - Is password equal to user.password
+            done(null, false) 
+        }
+    })
+
+})
+
+
+
+// -====================-
+// -=== JWT STRATEGY ===- 
+// -====================-
+
 
 // Setup options for JWT Strategy
 const jwtOptions = {
+    // If you want to test this with postman or RestClient
+    // -- Just create a custom header (authorization) and set it with a valide tokan from db
     jwtFromRequest: ExtractJwt.fromHeader('authorization'),
     secretOrKey: config.secret
 }
@@ -27,6 +81,7 @@ const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
     })
 
 })
+
 
 // Tell passport to use this strategy
 passport.use(jwtLogin)
